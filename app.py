a842340f5238a2cd05b27520e73f302093700dc8
Aleksandar Colovic <aleksandar.colovic@gmail.com>
2023-01-30 18:02:21 +0100

    Added amqp conection pooling

--- app.py
+++ app.py
@@ -5,31 +5,47 @@ import uuid
 import sys
 import aioredis
 import time
+from aio_pika.abc import AbstractRobustConnection
+from aio_pika.pool import Pool
 
 COMMENTS_REGEX = r'/comments/?'
 COMMENTS_VIDEO_REGEX = r'/comments/(?P<id>[a-zA-Z0-9-]+)/?'
 
 async def main():
-    app = make_app()
-    app.listen(3000)
+    loop = asyncio.get_event_loop()
 
-    await asyncio.Event().wait()
+    async def get_connection() -> AbstractRobustConnection:
+        return await aio_pika.connect_robust("amqp://guest:guest@localhost/")
+
+    amqp_connection_pool: Pool = Pool(get_connection, max_size=1000, loop=loop)
+
+    async def get_channel() -> aio_pika.Channel:
+        async with amqp_connection_pool.acquire() as connection:
+            return await connection.channel()
+
+    amqp_channel_pool: Pool = Pool(get_channel, max_size=20, loop=loop)
+
+    async with amqp_connection_pool, amqp_channel_pool:
+      urls = [(COMMENTS_VIDEO_REGEX, CommentsByVideoHandler)]
+      app = Application(urls, amqp_connection_pool=amqp_connection_pool, amqp_channel_pool=amqp_channel_pool)
+      app.listen(3000)
+      await asyncio.Event().wait()
+    
 
 
 class CommentsByVideoHandler(RequestHandler):
   async def sendRequest(self, id):
-    self.connection = await aio_pika.connect("amqp://guest:guest@localhost/")
-    self.channel = await self.connection.channel()
-    self.callback_queue = await self.channel.declare_queue(exclusive=True)
+    amqp_channel_pool = self.application.settings["amqp_channel_pool"]
+    async with amqp_channel_pool.acquire() as channel:  # type: aio_pika.Channel
+      self.callback_queue = await channel.declare_queue(exclusive=True)
+      self.corr_id = str(uuid.uuid4())
+      self.future = asyncio.get_running_loop().create_future()
 
-    self.corr_id = str(uuid.uuid4())
-    self.future = asyncio.get_running_loop().create_future()
-
-    await self.channel.default_exchange.publish(
-      message=aio_pika.Message(body=bytes(id, encoding='utf-8'), reply_to=self.callback_queue.name, correlation_id=self.corr_id),
-      routing_key='task_queue')
-            
-    await self.callback_queue.consume(self.on_reply)
+      await channel.default_exchange.publish(
+        message=aio_pika.Message(body=bytes(id, encoding='utf-8'), reply_to=self.callback_queue.name, correlation_id=self.corr_id),
+        routing_key='task_queue')
+              
+      await self.callback_queue.consume(self.on_reply)
 
   async def get(self, id):
     start = time.time()
@@ -46,10 +62,8 @@ class CommentsByVideoHandler(RequestHandler):
     if  message.correlation_id == self.corr_id:
       self.future.set_result(message.body)
 
-def make_app():
-  urls = [(COMMENTS_VIDEO_REGEX, CommentsByVideoHandler)]
-  return Application(urls)
-  
+
+
 if __name__ == '__main__':
   
   try:
